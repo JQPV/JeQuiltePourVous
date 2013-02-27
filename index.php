90bebedfd352892511ad991fe74aa0d861526c75
OpenDir <opendir@vmail.me>
2013-02-27 09:40:36 +0100

    PHP Mode

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<!-- Website Template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Paint Republic Website Template</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<?php include "menu.php" ?>
+	<?php include "body.php" ?>
+	<?php include "footer.php" ?>
+</body>
+</html>
